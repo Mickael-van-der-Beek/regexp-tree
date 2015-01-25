89b334d1cab7463a88232c2122273b932047f467
Mickael van der Beek <mickael@sagacify.com>
2015-01-25 01:36:19 +0100

    Initial version of the group parser.

--- src/regexp-tree.js
+++ src/regexp-tree.js
@@ -5,7 +5,113 @@ module.exports = (function () {
 
 	function RegExpTree () {}
 
+	RegExpTree.prototype.parseGroup = function (group) {
+		console.log('++0.1');
+		var tree = [];
+
+		var currChar = null;
+
+		var hexadecimalBoundary = null;
+		var unicodeBoundary = null;
+
+		var groupStart = null;
+		var setStart = null;
+
+		var escape = null;
+
+		for (var i = 0, len = group.length; i < len; i++) {
+			currChar = group[i];
+
+			console.log('++1');
+			console.log('CHAR=', currChar);
+			console.log('TREE=', tree);
+
+			if (escape === null && currChar === '\\') {
+				console.log('++2');
+				escape = i;
+			}
+			else if (escape === null && currChar === '(' && groupStart === null) {
+				console.log('++3');
+				groupStart = i;
+			}
+			else if (escape === null && currChar === ')' && groupStart !== null) {
+				console.log('++4');
+				tree.push(
+					this.parseGroup(
+						group.substr(setStart + 1, i - setStart - 1)
+					)
+				);
+				groupStart = null;
+			}
+			else if (escape === null && currChar === '[' && setStart === null && groupStart === null) {
+				console.log('++5');
+				setStart = i;
+			}
+			else if (escape === null && currChar === ']' && setStart !== null) {
+				console.log('++6');
+				tree.push(
+					this.parseSet(
+						group.substr(setStart + 1, i - setStart - 1)
+					)
+				);
+				setStart = null;
+			}
+			else if (escape !== null && currChar === 'x' && hexadecimalBoundary === null) {
+				console.log('++7');
+				hexadecimalBoundary = i + 2;
+			}
+			else if (escape !== null && currChar === 'u' && unicodeBoundary === null) {
+				console.log('++8');
+				unicodeBoundary = i + 4;
+			}
+			else if (escape === null && helpers.isMetaCharacter(currChar) && setStart === null && groupStart === null) {
+				console.log('++9');
+				throw 'Unescaped ' + currChar + ' character.';
+			}
+			else if (escape !== null && !helpers.isMetaCharacter(currChar)) {
+				console.log('++10');
+				throw 'Escaping non meta-character ' + currChar + '.';
+			}
+
+			if (escape === null && hexadecimalBoundary === i) {
+				console.log('++11');
+				currChar = group.substr(hexadecimalBoundary - 3, 4);
+			}
+			else if (escape === null && unicodeBoundary === i) {
+				console.log('++12');
+				currChar = group.substr(unicodeBoundary - 1, 2);
+			}
+
+			if (escape === null && unicodeBoundary === i || hexadecimalBoundary === i) {
+				console.log('++13');
+				if (!helpers.isHexadecimal(currChar)) {
+					throw currChar + ' is invalid hexadecimal character.';
+				}
+				currChar = String.fromCharCode(parseInt(currChar, 16));
+				unicodeBoundary = null;
+			}
+
+			if (escape !== null && escape + 1 === i) {
+				console.log('++14');
+				escape = null;
+			}
+
+			if (escape === null &&
+				hexadecimalBoundary === null &&
+				unicodeBoundary === null &&
+				(groupStart === null && currChar !== ')') &&
+				(setStart === null && currChar !== ']')
+			) {
+				console.log('++15');
+				tree.push(currChar);
+			}
+		}
+
+		return tree;
+	};
+
 	RegExpTree.prototype.parseSet = function (set) {
+		console.log('++0.2');
 		var tree = [];
 
 		var currChar = null;
@@ -53,7 +159,7 @@ module.exports = (function () {
 				unicodeBoundary = null;
 			}
 
-			if (escape + 1 === i) {
+			if (escape !== null && escape + 1 === i) {
 				escape = null;
 			}
 
--- test/test.js
+++ test/test.js
@@ -3,21 +3,34 @@ var regExpTree = new RegExpTree();
 
 var tree;
 
+// var trees = [
+// 	'5',
+// 	'a-b',
+// 	'a-bc',
+// 	// '^a-bc',
+// 	'\\u0061-\\u0063\\u0065-\\u0067',
+// 	'a-\\u0061',
+// 	'a\\-\\u0061',
+// 	'\\u0061',
+// 	'\\\\',
+// 	'\\]',
+// 	'\\a',
+// 	'a--\\u0061'
+// ].forEach(function (set) {
+// 	tree = regExpTree.parseSet(set);
+// 	console.log('\nSET=', set);
+// 	console.log('TREE=', tree, '\n');
+// });
+
 var trees = [
-	'5',
-	'a-b',
-	'a-bc',
-	// '^a-bc',
-	'\\u0061-\\u0063\\u0065-\\u0067',
-	'a-\\u0061',
-	'a\\-\\u0061',
-	'\\u0061',
-	'\\\\',
-	'\\]',
-	'\\a',
-	'a--\\u0061'
-].forEach(function (set) {
-	tree = regExpTree.parseSet(set);
-	console.log('\nSET=', set);
-	console.log('TREE=', tree, '\n');
+	'(ab)',
+	'(a[c-d])',
+].forEach(function (group) {
+	tree = regExpTree.parseGroup(group);
+	console.log('\nGROUP=', group);
+	console.log('TREE=', require('util').inspect(tree, {
+		depth: 30,
+		colors: true
+	}));
 });
+
